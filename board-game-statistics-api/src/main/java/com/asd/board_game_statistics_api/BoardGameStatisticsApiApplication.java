d227a70d30b41611c89a34334fc8817cee8d5619
Daniel Keith <danielrkeith@Daniels-MacBook-Air.local>
2025-08-07 15:52:31 +1000

    backend init

---
+++ board-game-statistics-api/src/main/java/com/asd/board_game_statistics_api/BoardGameStatisticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.asd.board_game_statistics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameStatisticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameStatisticsApiApplication.class, args);
+	}
+
+}
---
+++ board-game-statistics-api/src/test/java/com/asd/board_game_statistics_api/BoardGameStatisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asd.board_game_statistics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameStatisticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
