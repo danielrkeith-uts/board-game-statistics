ab408f03cfccd0a3be9e0ae87b6d5c0bac29ee44
danielrkeith-uts <daniel.r.keith@student.uts.edu.au>
2025-08-14 15:10:33 +1000

    Hello world fetch (#2)
    Co-authored-by: Daniel Keith <danielrkeith@Daniels-MacBook-Air-3.local>

---
+++ board-game-statistics-api/src/main/java/com/asd/board_game_statistics_api/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.asd.board_game_statistics_api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello, world!";
+    }
+
+}
---
+++ board-game-statistics/src/HelloWorldComponent.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+const HelloWorldComponent = () => {
+    const [text, setText] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/hello-world")
+            .then(response => response.text())
+            .then(data => setText(data));
+    }, []);
+
+    return (
+        <p>{text}</p>
+    )
+}
+
+export {HelloWorldComponent}
