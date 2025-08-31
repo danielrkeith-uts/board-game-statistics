f29517e47fb3a1b2d3d09c6d50fce11dd8dacfd4
Matt <matthew@adler.id.au>
2025-08-31 13:55:49 +1000

    Feat: Flesh out groups dashboard

--- board-game-statistics/src/pages/group/GroupDropDown.tsx
+++ board-game-statistics/src/pages/group/GroupDropDown.tsx
@@ -1,6 +1,6 @@
 const GroupDropDown = () => {
   return (
-    <select name="" id="">
+    <select className="form-select mt-3" name="" id="">
         <option value="1">Group 1</option>
         <option value="2">Group 2</option>
         <option value="3">Group 3</option>
--- board-game-statistics/src/pages/group/GroupView.tsx
+++ board-game-statistics/src/pages/group/GroupView.tsx
@@ -1,33 +1,40 @@
-import GroupDropDown from './GroupDropDown'
+import { Tabs, Tab } from 'react-bootstrap'
+import './styles.css'
+import GroupHomeView from './GroupHomeView'
 
 const GroupView = () => {
   return (
-    <div className="mainContainer">
+    <>
+      <div className="container mt-4 px-0 d-flex justify-content-end gap-3">
+        <button className='btn btn-sm btn-success'>Create new group</button>
+        <button className='btn btn-sm btn-danger'>Leave group</button>
+      </div>
 
-    </div>
+      <div className="container">
+        <Tabs
+          defaultActiveKey="home"
+          id="uncontrolled-tab-example"
+          className="mb-3"
+        >
+          <Tab eventKey="home" title="Home">
+            <GroupHomeView />
+          </Tab>
+          <Tab eventKey="leaderboard" title="Leaderboard">
+            Leaderboard
+          </Tab>
+          <Tab eventKey="members" title="Members">
+            Members list
+          </Tab>
+          <Tab eventKey="invite" title="Invite">
+            Members list
+          </Tab>
+          <Tab eventKey="manage" title="Manage">
+            Manage perms and kick users?
+          </Tab>
+        </Tabs>
+      </div>
+    </>
   )
 }
 
 export default GroupView
-
-/* 
-<>
-      <div className="gridContainer">
-        <div className="leftPanel">
-          <GroupDropDown />
-          <ul>
-            <li>Game</li>
-            <li>Game</li>
-            <li>Game</li>
-          </ul>
-        </div>
-        <div className="memberList">
-          <ul>
-            <li>Member</li>
-            <li>Member</li>
-            <li>Member</li>
-          </ul>
-        </div>
-      </div>
-    </>
-*/
