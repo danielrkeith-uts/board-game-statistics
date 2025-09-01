dfaea3ea85fd941e68b81a786c50286fd271de43
Matthew Adler <38673971+LeftsideMartian@users.noreply.github.com>
2025-09-01 13:54:57 +1000

    Tweaks for group page + create and leave group modals
    * Build: Install React Router DOM
    
    * Chore: Delete template files
    
    * Feat: Setup React Router and Header component
    
    * Fix: Pipeline error
    
    * Build: Install bootstrap
    
    * Task: Restyle header with Bootstrap
    
    * Task: Change icon and small HomeView content
    
    * Task: Create mainContainer class
    
    * Build: Update README and add sass command to package.json
    
    * Build: Install React-Bootstrap
    
    * Feat: Flesh out groups dashboard
    
    * Build: Delete unused CSS files
    
    * Feat: Implement modals for create and leave group
    
    * Chore: Fix azure pipeline error

--- board-game-statistics/src/components/Header.tsx
+++ board-game-statistics/src/components/Header.tsx
@@ -1,25 +1,26 @@
 import { Link } from 'react-router-dom'
-import { CREATE_ACCOUNT_PAGE_URL, GAMES_PAGE_URL, GROUPS_PAGE_URL, HOME_PAGE_URL, LOGIN_PAGE_URL, MANAGE_ACCOUNT_PAGE_URL } from '../utils/constants'
-import './styles.css'
+import { GAMES_PAGE_URL, GROUPS_PAGE_URL, HOME_PAGE_URL, LOGIN_PAGE_URL } from '../utils/constants'
 
 const Header = () => {
-    return (
-        <div className='nav'>
-            <div className="logoContainer headerContainer">
-                Logo
-                <li className='active navLink'><Link to={HOME_PAGE_URL}>Home</Link></li>
-            </div>
-
-            <ul className='headerContainer'>
-                {/* Manage account should be dynamic based on if user is logged in */}
-                <li className='navLink'><Link to={MANAGE_ACCOUNT_PAGE_URL}>Manage Account</Link></li>
-                <li className='navLink'><Link to={GAMES_PAGE_URL}>Games</Link></li>
-                <li className='navLink'><Link to={GROUPS_PAGE_URL}>Groups</Link></li>
-                <li className='navLink'><Link to={LOGIN_PAGE_URL}>Login</Link></li>
-                <li className='navLink'><Link to={CREATE_ACCOUNT_PAGE_URL}>Create Account</Link></li>
-            </ul>
-        </div>
-    )
+	return (
+		<nav className='navbar navbar-expand bg-primary-subtle'>
+			<div className="container">
+				<span className='navbar-brand'>Logo</span>
+				<div className='collapse navbar-collapse' id='navbarNav'>
+					<ul className='navbar-nav'>
+						<li className='nav-item'><Link className='nav-link' to={HOME_PAGE_URL}>Home</Link></li>
+						{/* Manage account should be dynamic based on if user is logged in */}
+						{/* <li className='nav-item'><Link className='nav-link btn-primary' to={MANAGE_ACCOUNT_PAGE_URL}>Manage Account</Link></li> */}
+						<li className='nav-item'><Link className='nav-link' to={GAMES_PAGE_URL}>Games</Link></li>
+						<li className='nav-item'><Link className='nav-link' to={GROUPS_PAGE_URL}>Groups</Link></li>
+					</ul>
+				</div>
+				<ul className='navbar-nav'>
+					<li className='nav-item'><Link className='nav-link' to={LOGIN_PAGE_URL}>Login</Link></li>
+				</ul>
+			</div>
+		</nav>
+	)
 }
 
 export default Header
--- board-game-statistics/src/pages/home/HomeView.tsx
+++ board-game-statistics/src/pages/home/HomeView.tsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom"
+import { CREATE_ACCOUNT_PAGE_URL } from "../../utils/constants"
+
 const HomeView = () => {
   return (
-    <div>HomeView</div>
+    <>
+      <div className="mt-5 container d-flex flex-column justify-content-center align-items-center">
+        <h1>Board Games Statistics</h1>
+        <h4 className="mt-0">Track games with your friends!</h4>
+        <Link to={CREATE_ACCOUNT_PAGE_URL}><button className="btn btn-primary mt-3">Create account</button></Link>
+      </div>
+    </>
   )
 }
 
