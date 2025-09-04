ac6ef09b2f2865b487f27c9f87c07d1ebd49a34f
tek-f <aaron.a.falco@student.uts.edu.au>
2025-09-04 14:34:56 +1000

    Update MembersListView.tsx
    - Removed Members list table from MembersListView.tsx

--- board-game-statistics/src/pages/group/MembersListView.tsx
+++ board-game-statistics/src/pages/group/MembersListView.tsx
@@ -3,22 +3,23 @@ import InviteMemberView from "./InviteMemberView.tsx";
 const MembersListView = () => {
     return(
         <div>
-            <div>
-                <table className={'table'}>
-                    <thead>
-                        <tr><th>Group Member</th></tr>
-                    </thead>
-                    <tbody>
-                        <tr>Brian Johnson</tr>
-                        <tr>Ashley Smith</tr>
-                        <tr>Xuan Wu</tr>
-                        <tr>Thor Odinson</tr>
-                    </tbody>
-                </table>
-            </div>
+            {/*<div>*/}
+            {/*    <table className={'table'}>*/}
+            {/*        <thead>*/}
+            {/*            <tr><th>Group Member</th></tr>*/}
+            {/*        </thead>*/}
+            {/*        <tbody>*/}
+            {/*            <tr>Brian Johnson</tr>*/}
+            {/*            <tr>Ashley Smith</tr>*/}
+            {/*            <tr>Xuan Wu</tr>*/}
+            {/*            <tr>Thor Odinson</tr>*/}
+            {/*        </tbody>*/}
+            {/*    </table>*/}
+            {/*</div>*/}
             <div>
                 <InviteMemberView/>
             </div>
+            <br/>
         </div>
     )
 }
