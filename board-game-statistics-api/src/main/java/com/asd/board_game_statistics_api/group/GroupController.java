5504dd7dd63a2670062baecda42b610e139f812d
Matt <matthew@adler.id.au>
2025-09-11 17:39:27 +1000

    Task: Create GroupController

---
+++ board-game-statistics-api/src/main/java/com/asd/board_game_statistics_api/group/GroupController.java
@@ -0,0 +1,29 @@
+package com.asd.board_game_statistics_api.group;
+
+import com.asd.board_game_statistics_api.group.dto.CreateGroupRequest;
+import com.asd.board_game_statistics_api.model.Group;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/group")
+public class GroupController {
+    @Autowired
+    private GroupService groupService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createNewGroup(@RequestBody CreateGroupRequest createGroupRequest) {
+        groupService.createGroup(createGroupRequest.groupName(), createGroupRequest.creatorId());
+        return ResponseEntity.ok("Group created successfully");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Group>> getGroupsByUserId(int userId) {
+        List<Group> groups = groupService.getGroupsByUserId(userId);
+        return ResponseEntity.ok(List.of());
+    }
+}
