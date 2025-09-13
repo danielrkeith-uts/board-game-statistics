76b8ee50455a7cbc96260d230474014d089b4672
Matt <matthew@adler.id.au>
2025-09-13 19:55:02 +1000

    Chore: Generalise EmptyGroupException

--- board-game-statistics-api/src/main/java/com/asd/board_game_statistics_api/group/exceptions/EmptyGroupException.java
+++ board-game-statistics-api/src/main/java/com/asd/board_game_statistics_api/group/exceptions/EmptyGroupException.java
@@ -1,7 +1,7 @@
 package com.asd.board_game_statistics_api.group.exceptions;
 
 public class EmptyGroupException extends GroupException {
-    public EmptyGroupException() {
-        super("Leaving this group is not allowed as you are the last member.");
+    public EmptyGroupException(String message) {
+        super(message);
     }
 }
