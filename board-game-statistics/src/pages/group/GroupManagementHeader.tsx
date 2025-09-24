50129d9bddcc7c59413c477344438d231ecba13e
aaron-falco <aaron.a.falco@student.uts.edu.au>
2025-09-24 17:07:06 +1000

    Re-dded invite code input to front-end

--- board-game-statistics/src/pages/group/GroupManagementHeader.tsx
+++ board-game-statistics/src/pages/group/GroupManagementHeader.tsx
@@ -2,6 +2,7 @@ import type { ChangeEvent } from 'react';
 import type { Group } from '../../utils/types';
 import GroupDropDown from './GroupDropDown';
 import CreateGroupButton from './CreateGroupButton';
+import InviteMemberView from './InviteMemberView.tsx';
 
 interface GroupManagementHeaderProps {
 	groups: Group[];
@@ -21,20 +22,25 @@ const GroupManagementHeader = (props: GroupManagementHeaderProps) => {
 	} = props;
 
 	return (
-		<div className="container mb-3">
-			<div className="row">
-				<div className="col-3">
+		<div className='container mb-3'>
+			<div className='row'>
+				<div className='col-3'>
 					<GroupDropDown
 						groups={groups}
 						currentGroup={currentGroup}
 						onChange={handleGroupDropdownChange}
 					/>
+					<br />
+					<InviteMemberView group={currentGroup} />
 				</div>
-				<div className="col">
-					<div className="container mt-4 px-0 d-flex justify-content-end gap-3">
-						<CreateGroupButton onClick={handleOpenCreateGroupModal} size="sm" />
+				<div className='col'>
+					<div className='container mt-4 px-0 d-flex justify-content-end gap-3'>
+						<CreateGroupButton
+							onClick={handleOpenCreateGroupModal}
+							size='sm'
+						/>
 						<button
-							className="btn btn-sm btn-danger"
+							className='btn btn-sm btn-danger'
 							onClick={handleOpenLeaveGroupModal}
 						>
 							Leave group
