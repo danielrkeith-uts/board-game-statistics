358f49ed224d56e056178a2147981a9563cc3bdf
RayKinjo129 <ray.kinjo@student.uts.edu.au>
2025-09-26 23:02:29 +1000

    Add SignupView, header Create account link, routes, and account API export

--- board-game-statistics/src/App.tsx
+++ board-game-statistics/src/App.tsx
@@ -10,9 +10,10 @@ import {
 } from './utils/constants';
 import HomeView from './pages/home/HomeView';
 import GroupView from './pages/group/GroupView';
+import LoginView from './pages/login/LoginView';
 import { AccountContextProvider } from './context/AccountContext';
 import GamesView from './pages/games/GamesView';
-import LoginView from './pages/login/LoginView';
+import SignupView from './pages/signup/SignupView';
 
 function App() {
 	return (
@@ -20,13 +21,14 @@ function App() {
 			<Header />
 			<div className='m-4'>
 				<Routes>
-					{/* PLEASE PLUG IN YOUR PAGE VIEW AS AN ELEMENT INTO YOUR ROUTE */}
 					<Route path={HOME_PAGE_URL} element={<HomeView />} />
-					<Route path={MANAGE_ACCOUNT_PAGE_URL} />
 					<Route path={GAMES_PAGE_URL} element={<GamesView />} />
 					<Route path={GROUPS_PAGE_URL} element={<GroupView />} />
 					<Route path={LOGIN_PAGE_URL} element={<LoginView />} />
-					<Route path={CREATE_ACCOUNT_PAGE_URL} />
+					<Route
+						path={CREATE_ACCOUNT_PAGE_URL}
+						element={<SignupView />}
+					/>
 				</Routes>
 			</div>
 		</AccountContextProvider>
