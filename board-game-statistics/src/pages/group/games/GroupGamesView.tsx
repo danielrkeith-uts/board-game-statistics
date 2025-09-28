038c0417968d0f08e123822922a8d7c0decbd5f4
F0rklift-Certified <noah.j.bondoc@student.uts.edu.au>
2025-09-28 17:57:50 +1000

    moved the eslint

--- board-game-statistics/src/pages/group/games/GroupGamesView.tsx
+++ board-game-statistics/src/pages/group/games/GroupGamesView.tsx
@@ -43,7 +43,6 @@ const GroupGamesView = (props: GroupGamesViewProps) => {
 
 	useEffect(() => {
 		fetchRecords();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [group.id]);
 
 	return (
