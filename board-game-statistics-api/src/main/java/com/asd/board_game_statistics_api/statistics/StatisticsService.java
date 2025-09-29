e9f3e06821731b92ec6cf993e77fecdf0e0ed6ab
Matt <matthew@adler.id.au>
2025-09-29 21:31:22 +1000

    Test: Create StatisticsServiceTests

--- board-game-statistics-api/src/main/java/com/asd/board_game_statistics_api/statistics/StatisticsService.java
+++ board-game-statistics-api/src/main/java/com/asd/board_game_statistics_api/statistics/StatisticsService.java
@@ -1,6 +1,8 @@
 package com.asd.board_game_statistics_api.statistics;
 
 import com.asd.board_game_statistics_api.games.PostgreSqlGameRecordRepository;
+import com.asd.board_game_statistics_api.group.GroupService;
+import com.asd.board_game_statistics_api.group.exceptions.GroupException;
 import com.asd.board_game_statistics_api.model.PlayerResult;
 import com.asd.board_game_statistics_api.statistics.dto.PlayerStatisticResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +13,15 @@ import java.util.List;
 @Service
 public class StatisticsService implements IStatisticsService {
     @Autowired
+    private GroupService groupService;
+    @Autowired
     private PostgreSqlGameRecordRepository gameRecordRepository;
 
     public PlayerStatisticResponse getPlayerStatsByGroupId(Integer accountId, Integer groupId) {
+        if (!groupService.belongsToGroup(accountId, groupId)) {
+            throw new GroupException("Account does not belong to group.");
+        }
+
         List<PlayerResult> playerResults = gameRecordRepository.getPlayerResultsByGroup(accountId, groupId);
 
         int wins = 0;
---
+++ board-game-statistics-api/src/test/java/com/asd/board_game_statistics_api/statistics/StatisticsServiceTests.java
@@ -0,0 +1,36 @@
+package com.asd.board_game_statistics_api.statistics;
+
+import com.asd.board_game_statistics_api.group.exceptions.GroupException;
+import com.asd.board_game_statistics_api.statistics.dto.PlayerStatisticResponse;
+import com.asd.board_game_statistics_api.test_utils.TestsWithMockedDatabase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StatisticsServiceTests extends TestsWithMockedDatabase {
+    @Autowired
+    StatisticsService statisticsService;
+
+    @Test
+    public void doesGetPlayerStatsByGroupIdErrorForInvalidGroupId() {
+        int accountId = 1;
+        int groupId = -1;
+
+        Assertions.assertThrows(
+                GroupException.class,
+                () -> statisticsService.getPlayerStatsByGroupId(accountId, groupId)
+        );
+    }
+
+    @Test public void doesGetPlayerStatsByGroupIdReturnResponse() {
+        int accountId = 1;
+        int groupId = 1;
+
+        PlayerStatisticResponse response = statisticsService.getPlayerStatsByGroupId(accountId, groupId);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(3, response.numOfGamesPlayed());
+        Assertions.assertEquals(1, response.wins());
+        Assertions.assertEquals(2, response.losses());
+    }
+}
