1e700ab3aee2f1af3ed33eef812a73a272e8940c
raykaikinjo <139049907+raykaikinjo@users.noreply.github.com>
2025-09-29 16:42:58 +1000

    Signup clean (#27)
    * Add SignupView, routes, and account API export
    
    * apiCreateAccount fixed
    
    ---------
    
    Co-authored-by: RayKinjo129 <ray.kinjo@student.uts.edu.au>

--- board-game-statistics/src/pages/home/HomeView.tsx
+++ board-game-statistics/src/pages/home/HomeView.tsx
@@ -11,11 +11,13 @@ const HomeView = () => {
 			<div className='container d-flex flex-column justify-content-center align-items-center mb-3'>
 				<h1>Board Games Statistics</h1>
 				<h4 className='mt-0'>Track games with your friends!</h4>
-				<Link to={CREATE_ACCOUNT_PAGE_URL}>
-					<button className='btn btn-primary mt-3'>
-						Create account
-					</button>
-				</Link>
+				{!account && (
+					<Link to={CREATE_ACCOUNT_PAGE_URL}>
+						<button className='btn btn-primary mt-3'>
+							Create account
+						</button>
+					</Link>
+				)}
 				{account &&
 					(account.firstName ? (
 						<p className='mt-3'>Welcome, {account?.firstName}!</p>
